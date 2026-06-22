8543867f958e997e9eaa617564cba1eb0ec622bf
FrancoMaxi25 <franco.maxi@tecsup.edu.pe>
2026-06-22 16:59:13 -0500

    feat: agregar sitemaps dedicados en la raiz de pages

---
+++ pages/sitemap-home.xml.js
@@ -0,0 +1,16 @@
+const BASE_URL = "https://mi-proyecto-seo-maxi.onrender.com";
+
+export async function getServerSideProps({ res }) {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${BASE_URL}/</loc>
+  </url>
+</urlset>`;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+  return { props: {} };
+}
+export default function SitemapHome() { return null; }
